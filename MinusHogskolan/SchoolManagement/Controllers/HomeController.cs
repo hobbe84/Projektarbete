2e0cab86c3a72feeaa510f903f255fb232841574
hobbe84 <hobbe@bredband.net>
2015-04-08 15:51:21 +0200

    update

--- MinusHogskolan/SchoolManagement/Controllers/HomeController.cs
+++ MinusHogskolan/SchoolManagement/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace SchoolManagement.Controllers
             List<Courses> AllCourses;
 
             // Skapa koppling till databasen
-            using(var ctx = new MinushogskolanDbEntities())
+            using (var ctx = new MinushogskolanDbEntities())
             {
                 // Ta fram alla kurser ur tabellen Courses och sortera på namn.
                 AllCourses = ctx.Courses
@@ -106,12 +106,12 @@ namespace SchoolManagement.Controllers
                          Info = c.Info,
                          Points = c.Points
                      });
-                course = query.FirstOrDefault();                 
+                course = query.FirstOrDefault();
             }
             if (course == null)
             {
                 return HttpNotFound();
-            } 
+            }
             return View(course);
         }
         [HttpPost]
@@ -125,14 +125,16 @@ namespace SchoolManagement.Controllers
                 {
                     var query = ctx.Courses
                         .FirstOrDefault(c => c.ID == id);
-                        
+
 
                     query.ActiveCourse = false;
                     ctx.SaveChanges();
                 }
+                TempData["success"] = string.Format("Kursen har avregistrerats");
             }
-            catch 
+            catch
             {
+                TempData["error"] = "Misslyckades att avregistrera kursen. Försök igen!";
                 return RedirectToAction("DeleteCourse", new { id = id });
             }
             return RedirectToAction("GetAllCourses");
@@ -140,7 +142,31 @@ namespace SchoolManagement.Controllers
         [HttpGet]
         public ActionResult DeleteStudent(int? id)
         {
-            return View();
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Students student;
+            using (var ctx = new MinushogskolanDbEntities())
+            {
+                var query =
+                    (from s in ctx.Students
+                     where s.ID == id
+                     select new Students
+                     {
+                         FirstName = s.FirstName,
+                         LastName = s.LastName,
+                         BirthDate = s.BirthDate,
+                         Address = s.Adress,
+                         City = s.City
+                     });
+                student = query.FirstOrDefault();
+            }
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
         }
         [HttpPost]
         [ActionName("DeleteStudent")]
@@ -158,9 +184,11 @@ namespace SchoolManagement.Controllers
                     query.ActiveStudent = false;
                     ctx.SaveChanges();
                 }
+                TempData["success"] = string.Format("Studenten är avregistrerad");
             }
             catch
             {
+                TempData["error"] = "Misslyckades att avregistrera studenten. Försök igen!";
                 return RedirectToAction("DeleteStudent", new { id = id });
             }
             return RedirectToAction("GetAllStudents");
@@ -168,7 +196,31 @@ namespace SchoolManagement.Controllers
         [HttpGet]
         public ActionResult DeleteTeacher(int? id)
         {
-            return View();
+            if (!id.HasValue)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Teachers teacher;
+            using(var ctx = new MinushogskolanDbEntities())
+            {
+                var query =
+                    (from t in ctx.Teachers
+                         where t.ID == id
+                         select new Teachers
+                         {
+                             FirstName = t.FirstName,
+                             LastName = t.LastName,
+                             BirthDate = t.BirthDate,
+                             Address = t.Adress,
+                             City = t.City
+                         });
+                teacher = query.FirstOrDefault();
+            }
+            if(teacher == null)
+            {
+                return HttpNotFound();
+            }
+            return View(teacher);
         }
         [HttpPost]
         [ActionName("DeleteTeacher")]
@@ -186,9 +238,11 @@ namespace SchoolManagement.Controllers
                     query.ActiveTeacher = false;
                     ctx.SaveChanges();
                 }
+                TempData["success"] = string.Format("Läraren är avregistrerad");
             }
             catch
             {
+                TempData["error"] = "Misslyckades att avregistrera läraren. Försök igen!";
                 return RedirectToAction("DeleteTeacher", new { id = id });
             }
             return RedirectToAction("GetAllTeachers");
@@ -197,8 +251,8 @@ namespace SchoolManagement.Controllers
 
 
 
-        
+
     }
 
-    
+
 }
